b26c590fb8c8366c82b8f9f00c6a8d48e24b7627
Tom Stuart <tom@codon.com>
2017-04-30 14:10:47 +0100

    Move dimmer code into Tradfri::Device

--- lib/tradfri/device.rb
+++ lib/tradfri/device.rb
@@ -4,6 +4,9 @@ module Tradfri
     ON_OFF = 5850
     OFF = 0
     ON = 1
+    DIMMER = 5851
+    DIMMER_MIN = 0
+    DIMMER_MAX = 255
 
     BULBS = 15001 # TODO discover this
 
@@ -17,6 +20,10 @@ module Tradfri
 
     def off
       gateway.send_command uri, ON_OFF => OFF
+    end
+
+    def dim(brightness)
+      gateway.send_command uri, DIMMER => DIMMER_MIN + (brightness * (DIMMER_MAX - DIMMER_MIN)).round
     end
   end
 end
